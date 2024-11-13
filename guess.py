ae7121eff2e1e6cf08525fbc83b8c6e003ab678a
semionvictor <semionvictor7@gmail.com>
2024-11-13 15:01:08 +0100

    application of automation

--- guess.py
+++ guess.py
@@ -1,18 +1,26 @@
 import random
+import sched
+import time
 
-random_number = random.randint(0,10)
-print("enter a number between 0 and  10")
-
-user_guess = int(input())
-
-while user_guess != random_number:
-    if user_guess > random_number:
-        print("you are above the number")
-
-    else:
-        print("you are below the number")
+def guess_game():
+    random_number = random.randint(0,10)
+    print("enter a number between 0 and  10")
 
     user_guess = int(input())
 
-if user_guess == random_number:
-    print("you got the correct answer😊😊")
+    while user_guess != random_number:
+        if user_guess > random_number:
+            print("you are above the number")
+
+        else:
+            print("you are below the number")
+
+        user_guess = int(input())
+
+    if user_guess == random_number:
+        print("you got the correct answer😊😊")
+
+
+scheduler = sched.scheduler(time.time, time.sleep)
+scheduler.enter(20, 1, guess_game)
+scheduler.run()
